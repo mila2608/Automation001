431791acc7c9df200accf42c74b47abc07c619de
Mila P <lyudmila.pa@gmail.com>
2018-06-10 14:01:45 +0300

    commit 2

--- src/test/java/company/Drivers/DriverFactory.java
+++ src/test/java/company/Drivers/DriverFactory.java
@@ -1,11 +1,29 @@
 package company.Drivers;
 
+import company.Common.Constats;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class DriverFactory {
     public static WebDriver getDriver(){
-
         WebDriver driver = null;
+        String name = System.getProperty("driver");
+
+        if ("firefox".equals(name))
+        {
+            System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
+        else {
+            System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(Constats.BASE_URL);
         return driver;
 
     }
--- src/test/java/company/Pages/BasePage.java
+++ src/test/java/company/Pages/BasePage.java
@@ -1,24 +1,23 @@
 package company.Pages;
 
-import company.Common.Constats;
+import company.Drivers.DriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
-    static protected WebDriver driver;
+    static protected WebDriver driver = DriverFactory.getDriver();
 
-    static {
-       /*  System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        driver = new ChromeDriver();*/
-       System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
-        driver = new FirefoxDriver();
-      /* System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
-        driver = new FirefoxDriver();*/
+   /* static {
+       *//*  System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        driver = new ChromeDriver();*//*
+       *//*System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+        driver = new FirefoxDriver();*//*
+      *//* System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+        driver = new FirefoxDriver();*//*
 
-        driver.get(Constats.BASE_URL);
+      //  driver.get(Constats.BASE_URL);
     }
-
+*/
     public BasePage() {
         PageFactory.initElements(driver, this);
     }
