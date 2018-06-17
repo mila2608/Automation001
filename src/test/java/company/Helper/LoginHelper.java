b8972ec5d42a4e678becab0d9341d4e38000d552
Mila P <lyudmila.pa@gmail.com>
2018-06-17 14:25:30 +0300

    commit 4

--- src/test/java/company/Helper/LoginHelper.java
+++ src/test/java/company/Helper/LoginHelper.java
@@ -6,6 +6,9 @@ import company.Utils.Users.User;
 public class LoginHelper extends LoginPage{
 
 public void login (User user){
+log.info(String.format("User login: userr -%s, password -%s",
+        user.getLogin(),
+        user.getPassword()));
     login(user.getLogin(), user.getPassword());
 }
 
